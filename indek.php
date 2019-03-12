dffac378ae7fb2eb7da40f53c5d5ac45be8b69ce
madasiregar85 <madasiregar.43@gmail.com>
2019-03-12 11:19:43 +0700

    lingkaran2

---
+++ indek.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<h1 style="color:#7EF9A3;">This is a Blue Heading</h1>
+<?php
+echo "<b style='color:red;'>ini warna merah</b>";
+echo "<h1 style='color:#7EF9A3;font-family:verdana;'>This is a blue heading <h1>";
+?>
+
+</body>
+</html>
